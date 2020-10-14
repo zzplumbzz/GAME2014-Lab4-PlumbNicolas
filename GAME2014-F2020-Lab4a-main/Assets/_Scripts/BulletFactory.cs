5eb543d217a08f3a2b9ec65ebafcfd82aeda448c
zzplumbzz <nicolasplumb@yahoo.com>
2020-10-14 14:38:37 -0400

    initial commit

---
+++ GAME2014-F2020-Lab4a-main/Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletFactory : MonoBehaviour
+{
+
+    [Header("BulletTypes")]
+    //Header("Bullet Types")]
+    public GameObject regularBullet;
+    public GameObject fatBullet;
+    public GameObject pulsingBullet;
+
+    public BulletManager bulletManager;
+
+
+
+    public GameObject createBullet(BulletType type)
+    {
+        GameObject tempBullet = new GameObject();
+        switch (type)
+        {
+
+            case BulletType.REGULAR:
+                Instantiate(regularBullet);
+                break;
+            case BulletType.FAT:
+                Instantiate(fatBullet);
+                break;
+            case BulletType.PULSING:
+                Instantiate(pulsingBullet);
+                break;
+        }
+        tempBullet.transform.parent = transform;
+
+        return tempBullet;
+    }
+}
+
